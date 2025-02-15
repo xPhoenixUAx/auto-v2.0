f51350da2b70ccd3664f1f5eb8d5b074bf7a8a48
Pavlo Mazur <161851925+xPhoenixUAx@users.noreply.github.com>
2025-02-15 04:51:41 +0100

    update cookie

--- script2.js
+++ script2.js
@@ -34,3 +34,23 @@ navLinks.querySelectorAll("a").forEach((link) => {
     menuToggle.classList.remove("active");
   });
 });
+window.onload = function() {
+  // Якщо cookies не були прийняті або відмовлено від них, показуємо модальне вікно
+  if (localStorage.getItem('cookiesAccepted') === null) {
+    document.getElementById('cookieModal').style.display = 'flex';
+  } else if (localStorage.getItem('cookiesAccepted') === 'false') {
+    document.getElementById('cookieModal').style.display = 'flex'; // Відображаємо, якщо була відмова
+  }
+
+  // Прийняття cookies
+  document.getElementById('acceptCookies').addEventListener('click', function() {
+    localStorage.setItem('cookiesAccepted', 'true'); // Зберігаємо інформацію про прийняття
+    document.getElementById('cookieModal').style.display = 'none'; // Закриваємо модальне вікно
+  });
+
+  // Відмова від cookies
+  document.getElementById('declineCookies').addEventListener('click', function() {
+    localStorage.setItem('cookiesAccepted', 'false'); // Зберігаємо інформацію про відмову
+    document.getElementById('cookieModal').style.display = 'none'; // Закриваємо модальне вікно
+  });
+};
